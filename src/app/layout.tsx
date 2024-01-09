86327ddec98926db142d53f7f060843c31e06029
Vikor Bogdán <vikorbogdan@gmail.com>
2024-01-09 22:47:16 +0100

    feat(product-details-page): Static Site Build
    Completed static site build from webdesign
    
    - Created dynamic route for product details page
    - Created responsive product details page
    - Added dependency for icons: Lucide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import generalSans from "./fonts/generalSans";
+import { ShoppingBasket } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Products Page",
@@ -13,7 +14,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${generalSans.className} bg-gray-100`}>{children}</body>
+      <body className={`${generalSans.className} bg-gray-100`}>
+        <header className="shadow-sm fixed z-50 bg-white h-14 w-full flex items-center px-2 md:px-14">
+          <a href="/" className="select-none cursor-pointer">
+            Products
+          </a>
+          <div className="ml-auto gap-4 flex">
+            <div className="relative">
+              <div className="text-center -bottom-1 -left-2 bg-red-500 text-white absolute px-2 text-xs font-medium rounded-full">
+                3
+              </div>
+              <button className="rounded-full transition-colors text-white bg-black hover:bg-primary w-10 h-10">
+                <ShoppingBasket className="mx-auto" />
+              </button>
+            </div>
+            <button className="bg-black hover:bg-primary transition-colors text-white px-5 py-2 rounded-full">
+              Sign In
+            </button>
+          </div>
+        </header>
+        <main className="flex h-full items-center justify-center">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
