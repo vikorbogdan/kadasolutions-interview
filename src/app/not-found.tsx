1cd2b9530c07c3f3da16350c6dcea52c7a4588fd
Vikor Bogdán <vikorbogdan@gmail.com>
2024-01-09 23:10:16 +0100

    feat(not-found-page): Added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="h-screen flex items-center justify-center text-center flex-col">
+      <h1 className="text-7xl font-semibold">404</h1>
+      <h1>Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
