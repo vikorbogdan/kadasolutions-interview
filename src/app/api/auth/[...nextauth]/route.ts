ea6913135d1f31a34c8382ab9bc323cfda234081
Vikor Bogdán <vikorbogdan@gmail.com>
2024-01-11 01:33:08 +0100

    fix(build): Changed up export syntax in nextauth options causing error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
+import { AuthOptions } from "next-auth"
 import NextAuth from "next-auth/next"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
+const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -11,4 +12,4 @@ export const authOptions = {
 }
 
 export const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST, authOptions }
